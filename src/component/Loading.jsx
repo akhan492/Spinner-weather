be38a80f92b88093b4b8829676a2934038ff8d44
akhan492 <87148816+akhan492@users.noreply.github.com>
2022-06-20 19:10:02 +0530

    Initial commit

---
+++ src/component/Loading.jsx
@@ -0,0 +1,11 @@
+import "./loading.css";
+function Loading() {
+  return (
+    <>
+      <div className="parent">
+        <div id="spin"></div>
+      </div>
+    </>
+  );
+}
+export default Loading;
